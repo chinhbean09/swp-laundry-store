1f0c8554db45b8a5cdc3f6e4450a006486974856
chinhbean09 <chinh0726@gmail.com>
2023-11-17 17:04:00 +0700

    Upload Store Controller

--- src/main/java/com/project/thelaundry/controllers/StoreController.java
+++ src/main/java/com/project/thelaundry/controllers/StoreController.java
@@ -19,6 +19,67 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 
 public class StoreController {
+    private final LaundryServiceImp service;
+    //    public StoreController(LaundryServiceImp laundryservice) {
+//        this.laundryservice = laundryservice;
+//    }
+    @GetMapping("/special-service/all")
+    @PreAuthorize("hasAuthority('store:read')")
+    public ResponseEntity<List<LaundryInfoDTO>> getAllSpecialServices(@RequestParam(name="store")  long id){
+        return ResponseEntity.ok(service.getAllSpecialServiceForStore(id));
+    }
 
+    @GetMapping("/standard-service/get")
+    @PreAuthorize("hasAuthority('store:read')")
+    public ResponseEntity<LaundryInfoDTO> getStandardServicesById(@RequestParam(name = "store") long id) {
+        return  ResponseEntity.ok(service.getStandardServiceForStore(id));
+    }
+
+
+
+
+
+
+
+
+
+    @PutMapping("/special-service/update/{id}")
+    @PreAuthorize("hasAuthority('store:update')")
+    public ResponseEntity<LaundryInfoDTO> updateSpecialService(@RequestBody SpecialServiceRequest request,@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(service.updateSpecialService(request,id));
+    }
+
+    @DeleteMapping("/special-service/delete/{id}")
+    @PreAuthorize("hasAuthority('store:delete')")
+    public ResponseEntity deleteSpecialService(@PathVariable(name = "id") long id) {
+        service.deleteService(id);
+        return  ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/standard-service/update/{id}")
+    @PreAuthorize("hasAuthority('store:update')")
+    public ResponseEntity<LaundryInfoDTO> updateStandardService(@RequestBody StandardServiceRequest request,@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(service.updateStandardService(request,id));
+    }
+
+
+
+    @DeleteMapping("/standard-service/delete/{id}")
+    @PreAuthorize("hasAuthority('store:delete')")
+    public ResponseEntity deleteStandardService(@PathVariable(name = "id") long id) {
+        service.deleteService(id);
+        return ResponseEntity.ok().build();
+
+    }
+
+
+
+    @DeleteMapping("/standard-service/prices/delete/{id}")
+    @PreAuthorize("hasAuthority('store:delete')")
+    public ResponseEntity deletePricesStandardService(@PathVariable(name = "id") long id) {
+        service.deletePrice(id);
+        return ResponseEntity.ok().build();
+
+    }
 }
 
